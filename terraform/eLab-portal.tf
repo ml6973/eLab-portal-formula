d746f1a16576d1c6595e8f8886ea5119a3ce5b10
Daniel Curran <daniel.curran@rackspace.com>
2017-03-28 21:45:52 +0000

    updating terraform

---
+++ terraform/eLab-portal.tf
@@ -0,0 +1,36 @@
+provider "openstack" {
+    user_name  = "${var.user_name}"
+    tenant_name = "${var.tenant_name}"
+    password  = "${var.password}"
+    auth_url  = "${var.auth_url}"
+}
+
+resource "openstack_compute_floatingip_v2" "floatip_1" {
+  pool = "ext-net"
+}
+
+data "template_file" "salt_bootstrap_elab_web" {
+	template = "${file("salt_bootstrap.sh")}"
+	vars {
+		roles = "eLab_portal"
+		environment = "${var.environment}"
+                salt_master = "${var.salt_master}"
+	}
+}
+
+resource "openstack_compute_instance_v2" "web_node" {
+    name = "${var.node_name}"
+    image_id = "cf5974ec-0d3d-4206-8251-01d964efa20e"
+    flavor_id = "4"
+    key_pair = "jenkins_key"
+    security_groups = ["default"]
+    network {
+        name = "CH-816772-net"
+        floating_ip = "${openstack_compute_floatingip_v2.floatip_1.address}"
+    }
+    user_data = "${data.template_file.salt_bootstrap_elab_web.rendered}"
+}
+
+output "public_ip" {
+    value = "${openstack_compute_floatingip_v2.floatip_1.address}"
+}
